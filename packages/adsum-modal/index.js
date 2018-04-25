54dce6d51877f9d6f12c56cc065b25f1f390ca7c
Stanley <Stanley92@hotmail.com>
2018-04-25 14:12:26 +0800

    Implemented basic adsum modal component

---
+++ packages/adsum-modal/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+require('./modal.less');
+
+const AdsumModal = ({ isOpen, onCloseClicked, effect, children }) => (
+    <div className={`adsumModal ${isOpen ? 'open' : ''}`}>
+        <div className="adsumModal-overlay" onClickCapture={onCloseClicked}/>
+        <div className={`adsumModal-wrapper ${effect} show`}>
+            {children}
+        </div>
+    </div>
+);
+
+AdsumModal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onCloseClicked: PropTypes.func.isRequired,
+    effect: PropTypes.string
+};
+
+AdsumModal.defaultProps = {
+    effect: 'fadeIn'
+}
+
+export default AdsumModal;
