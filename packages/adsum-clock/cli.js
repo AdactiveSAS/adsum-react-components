5650945d3583ebdfefefc52715c996a86e4d7ca6
Gethi <bigard.antoine@gmail.com>
2018-05-09 18:08:33 +0800

    Copy whole component

--- packages/adsum-clock/cli.js
+++ packages/adsum-clock/cli.js
@@ -5,6 +5,12 @@
 const chalk = require('chalk');
 const fs = require('fs-extra');
 
-fs.copy(`${__dirname}/src/adsumClock.less`, path.resolve('src/components/adsum-clock/adsumClock.less'))
+/*fs.copy(`${__dirname}/src/adsumClock.less`, path.resolve('src/components/adsum-clock/adsumClock.less'))
+  .then(() => console.log(chalk.green("Success!")) )
+  .catch(err => console.error(err))*/
+
+
+  fs.copy(`${__dirname}/index.js`, path.resolve('src/components/adsum-clock/index.js'))
+  .then(() => fs.copy(`${__dirname}/src`, path.resolve('src/components/adsum-clock/src')))
   .then(() => console.log(chalk.green("Success!")) )
   .catch(err => console.error(err))
