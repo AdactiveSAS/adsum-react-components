db9637207625cda42ab0bf5248cb5881c64c4d83
Basile Nouvellet <basile.nouvellet@ponts.org>
2018-11-07 10:29:12 +0800

    Clean jest config file

--- packages/adsum-az-scroller/jest.config.js
+++ packages/adsum-az-scroller/jest.config.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-const { defaults } = require('jest-config'); // if default config is needed
-
 module.exports = {
     transformIgnorePatterns: ['/node_modules/.+\\.js$'],
     setupTestFrameworkScriptFile: '<rootDir>/src/setupTests.js',
